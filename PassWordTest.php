8308d1db263b48723dc13feb0e7999861bd21cf1
Estelle Croquet <estelle.croquet@student.vinci.be>
2022-01-21 09:28:38 +0100

    add - test testEightsCarractersGiven12345678MustResturnTrue

--- PassWordTest.php
+++ PassWordTest.php
@@ -2,5 +2,15 @@
 
 class PassWordTest
 {
+    public function testEightsCarractersGiven12345678MustResturnTrue(){
+        // Arrange
+        $PassWord = new PassWord();
+
+        // Act
+        $verrifyPassWord = $PassWord->verrifyPassWord(12345678);
+
+        //Assert
+        $this->assertEquals (TRUE, $verrifyPassWord);
+    }
 
 }
